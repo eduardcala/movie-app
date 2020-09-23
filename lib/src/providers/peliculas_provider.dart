9aaefe804471a7817305bf85e4196b9d78582843
Eduard Cala <eduardcala@gmail.com>
2020-09-22 23:01:39 -0500

    Creando un Stream de películas

--- lib/src/providers/peliculas_provider.dart
+++ lib/src/providers/peliculas_provider.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
@@ -9,6 +10,21 @@ class PeliculasProvider {
   String _apiKey   = 'd18414931ee9815f15b207f6bcaecc7e';
   String _url      = 'api.themoviedb.org';
   String _language = 'es-ES';
+
+  int _popularesPage = 0;
+
+  List<Pelicula> _populares = new List();
+
+  final _popularesStreamController = StreamController<List<Pelicula>>.broadcast();
+
+  Function(List<Pelicula>) get popularesSink => _popularesStreamController.sink.add;
+
+  Stream<List<Pelicula>> get popularesStream => _popularesStreamController.stream;
+
+
+  void disposeStreams() {
+    _popularesStreamController?.close();
+  }
 
 
   Future<List<Pelicula>> _procesarRespuesta(Uri url) async {
@@ -34,12 +50,20 @@ class PeliculasProvider {
 
   Future<List<Pelicula>> getPopulares() async {
 
+    _popularesPage++;
+
     final url = Uri.https(_url, '3/movie/popular', {
       'api_key'  : _apiKey,
-      'language' : _language
+      'language' : _language,
+      'page'     : _popularesPage.toString()
     });
 
-    return await _procesarRespuesta(url);
+    final resp = await _procesarRespuesta(url);
+
+    _populares.addAll(resp);
+    popularesSink(_populares);
+
+    return resp;
 
   }
 
