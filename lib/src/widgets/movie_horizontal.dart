6932239d571640a3179c85653388acd7886d20de
Eduard Cala <eduardcala@gmail.com>
2020-09-23 00:19:19 -0500

    112. Pasar argumentos de una página a otra usando el pushNamed

--- lib/src/widgets/movie_horizontal.dart
+++ lib/src/widgets/movie_horizontal.dart
@@ -41,7 +41,7 @@ class MovieHorizontal extends StatelessWidget {
   }
 
   Widget _tarjeta(BuildContext context, Pelicula pelicula){
-    return Container(
+    final tarjeta = Container(
         margin: EdgeInsets.only(right: 15.0),
         child: Column(
           children: [
@@ -63,6 +63,13 @@ class MovieHorizontal extends StatelessWidget {
           ],
         ),
       );
+
+    return GestureDetector(
+      child: tarjeta,
+      onTap: (){
+        Navigator.pushNamed(context, 'detalle', arguments: pelicula);
+      },
+    );
   }
 
   List<Widget> _tarjetas(BuildContext context) {
