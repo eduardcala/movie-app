203c8fe16344e946b130274276d0f920bd360992
Eduard Cala <eduardcala@gmail.com>
2020-09-24 00:22:54 -0500

    119. Hero Animation en las tarjetas principales

--- lib/src/pages/pelicula_detalle.dart
+++ lib/src/pages/pelicula_detalle.dart
@@ -65,7 +65,7 @@ class PeliculaDetalle extends StatelessWidget {
       child: Row(
         children: [
           Hero(
-            tag: pelicula.id,
+            tag: pelicula.uniqueId,
             child: ClipRRect(
               borderRadius: BorderRadius.circular(20.0),
               child: Image(
