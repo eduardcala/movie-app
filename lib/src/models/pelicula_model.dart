68a099fa6e70485fa6e86692a59b2fe467b788fe
Eduard Cala <eduardcala@gmail.com>
2020-09-23 21:50:08 -0500

    113. Diseño de la pantalla de detalle

--- lib/src/models/pelicula_model.dart
+++ lib/src/models/pelicula_model.dart
@@ -76,7 +76,7 @@ class Pelicula {
 
   getBackgroundImg() {
 
-    if ( posterPath == null) {
+    if ( backdropPath == null) {
       return 'https://thumbs.dreamstime.com/b/no-image-available-icon-photo-camera-flat-vector-illustration-132483141.jpg';
     }
     return 'https://image.tmdb.org/t/p/original/$backdropPath';
--- lib/src/widgets/movie_horizontal.dart
+++ lib/src/widgets/movie_horizontal.dart
@@ -72,37 +72,37 @@ class MovieHorizontal extends StatelessWidget {
     );
   }
 
-  List<Widget> _tarjetas(BuildContext context) {
+  // List<Widget> _tarjetas(BuildContext context) {
 
-    return peliculas.map((pelicula) {
+  //   return peliculas.map((pelicula) {
 
-      return Container(
-        margin: EdgeInsets.only(right: 15.0),
-        child: Column(
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(20.0),
-              child: FadeInImage(
-                placeholder: AssetImage('assets/img/no-image.jpg'),
-                image: NetworkImage(pelicula.getPosterImg()),
-                fit: BoxFit.cover,
-                height: 160.0,
-              ),
-            ),
-            SizedBox(height: 5.0,),
-            Text(
-              pelicula.title,
-              overflow: TextOverflow.ellipsis,
-              style: Theme.of(context).textTheme.caption,
-            )
-          ],
-        ),
-      );
+  //     return Container(
+  //       margin: EdgeInsets.only(right: 15.0),
+  //       child: Column(
+  //         children: [
+  //           ClipRRect(
+  //             borderRadius: BorderRadius.circular(20.0),
+  //             child: FadeInImage(
+  //               placeholder: AssetImage('assets/img/no-image.jpg'),
+  //               image: NetworkImage(pelicula.getPosterImg()),
+  //               fit: BoxFit.cover,
+  //               height: 160.0,
+  //             ),
+  //           ),
+  //           SizedBox(height: 5.0,),
+  //           Text(
+  //             pelicula.title,
+  //             overflow: TextOverflow.ellipsis,
+  //             style: Theme.of(context).textTheme.caption,
+  //           )
+  //         ],
+  //       ),
+  //     );
 
 
-    }).toList();
+  //   }).toList();
 
-  }
+  // }
 
 
 }
